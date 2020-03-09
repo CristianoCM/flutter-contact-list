6e5a3d5621876f7116a1653b8aa888238023b819
Cristiano <cristiano@blanko.be>
2020-03-09 17:20:38 -0300

    project

---
+++ lib/ui/contactPage.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+import 'package:contact_list/helpers/db.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  final _nameFocus = FocusNode();
+
+  bool _userEdited = false;
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.contact == null) {
+      _editedContact = Contact();
+    } else {
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
+    }
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Discard modifications?"),
+            content: Text("Closing the page will result in modifications data loss"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Cancel"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("Confirm"),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        }
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(_editedContact.name ?? "New Contact"),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+              Navigator.pop(context, _editedContact);
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+          child: Icon(Icons.save),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  width: 140.0,
+                  height: 140.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: _editedContact.img != null
+                            ? FileImage(File(_editedContact.img))
+                            : AssetImage("images/no_image.png")),
+                  ),
+                ),
+                onTap: () {
+                  ImagePicker.pickImage(
+                    source: ImageSource.gallery,
+                  ).then((file) {
+                    if (file == null) {
+                      return;
+                    }
+
+                    setState(() {
+                      _editedContact.img = file.path;
+                    });
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Name"),
+                onChanged: (value) {
+                  _userEdited = true;
+                  setState(() {
+                    _editedContact.name = value.isEmpty ? "New Contact" : value;
+                  });
+                },
+                focusNode: _nameFocus,
+                controller: _nameController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Email"),
+                onChanged: (value) {
+                  _userEdited = true;
+                  _editedContact.email = value;
+                },
+                keyboardType: TextInputType.emailAddress,
+                controller: _emailController,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Phone"),
+                onChanged: (value) {
+                  _userEdited = true;
+                  _editedContact.phone = value;
+                },
+                keyboardType: TextInputType.phone,
+                controller: _phoneController,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
